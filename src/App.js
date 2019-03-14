0edc476ab55515ba5ccea357e1796787f8e132ef
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-03-13 23:27:03 -0500

    starter image onMouseMove

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return {
+    transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
+  };
+}
 
 class App extends Component {
+  state = {
+    styleOne: {},
+    styleTwo: {}
+  };
+
+  onMouseMove = e => {
+    this.setState({
+      styleOne: transform(-e.clientX / e.clientY),
+      styleTwo: transform(e.clientX / e.clientY)
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div onMouseMove={this.onMouseMove}>
+        <div className="panel" style={this.state.styleOne} />
+        <div className="panel" style={this.state.styleTwo} />
       </div>
     );
   }
